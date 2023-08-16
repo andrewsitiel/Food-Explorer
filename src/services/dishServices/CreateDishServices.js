f2a04bdfb7381183c22d17bdae59048ccc54ba99
Andrews Itiel <andrewscarvalho2001@gmail.com>
2023-08-15 22:30:36 -0300

    Add validation and return values

--- src/services/dishServices/CreateDishServices.js
+++ src/services/dishServices/CreateDishServices.js
@@ -8,6 +8,11 @@ class CreateDishServices {
 
   async index(allIngredients) {
     const allDishes = await this.repository.select();
+
+    if (!allIngredients) {
+      return allDishes
+    }
+
     const allDishesWithIngredients = this.#utils.getAllDishesWithIngredients(allDishes, allIngredients);
 
     return allDishesWithIngredients
@@ -15,6 +20,11 @@ class CreateDishServices {
 
   async show(id, allIngredients) {
     const dish = await this.repository.show(id);
+
+    if (!allIngredients) {
+      return dish
+    }
+
     const dishWithIngredients = this.#utils.getDishWithIngredients(dish, allIngredients);
 
     return dishWithIngredients;
@@ -30,6 +40,28 @@ class CreateDishServices {
     await diskStorage.saveFile(image);
 
     return await this.repository.insert({ name, description, category, price, image });
+  }
+
+  async update({ id, name, description, category, price, image }) {
+    const oldDishInfos = await this.repository.show(id);
+
+    if (image) {
+      const { filename } = image;
+
+      const diskStorage = new DiskStorage();
+      await diskStorage.saveFile(filename);
+      await diskStorage.deleteFile(oldDishInfos.image)
+
+      await this.repository.patch({ id, filename });
+    }
+
+    return await this.repository.update({ id, name, description, category, price });
+  }
+
+  async delete(id) {
+    await this.repository.delete(id);
+
+    return
   }
 
   #utils = {
